9adc1a27212d4984aa8079c82822249791238d62
aparnasubhash2006-arch <aparnasubhash2006@gmail.com>
2026-07-03 10:19:49 +0530

    Fix title duplication and adjust input handling

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-
+st.title("Take the input")
 #Take a user input\
 name= st.text_input("enter your name")
 
-st.title("Take the input")
+
 
 if st.button("submit"):
   st.write(f"print the name: {name}")
